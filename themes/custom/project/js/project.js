142f5ef9d3dd090863fe3de60c1933f4eb906aed
Zeeshan Khan <princegill81@gmail.com>
2023-03-13 15:06:32 +0530

    initializing

---
+++ themes/custom/project/js/project.js
@@ -0,0 +1,28 @@
+(function (jQuery, Drupal, once) {
+    Drupal.behaviors.myConferences = {
+      attach: function attach(context, settings) {
+        var container = jQuery('.view-conferences .view-content .view-grouping');
+        var heading = container.find('.view-grouping-header');
+        
+        container.first().find('.slick--view').addClass('active');
+        container.first().find('.view-grouping-header').addClass('active');
+        container.find('.slick--view').addClass('tab-toggle');
+        container.find('.slick__slide .views-field-nothing .like:empty').removeClass('like');
+        container.find('.slick__slide .views-field-nothing .download:empty').removeClass('download');
+        container.find('.slick__slide .views-field-nothing .share:empty').removeClass('share');
+          
+         // container.find('.tab-toggle').hide();
+          //container.first().find('.tab-toggle').show();
+  
+        heading.on('click keypress', function (e) {
+          e.preventDefault();
+          jQuery('.slick--view').removeClass('active').addClass('tab-toggle');
+          heading.removeClass('active');
+          jQuery(this).addClass('active');
+          jQuery(this).next('.slick--view').removeClass('tab-toggle').addClass('active');
+          console.log('clicked');
+          
+        });
+      }
+    };
+  })(jQuery, Drupal, once);
